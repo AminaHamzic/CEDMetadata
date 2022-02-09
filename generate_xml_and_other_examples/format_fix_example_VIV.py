119af823495cb09ca293b865daac6b4068ab20e2
joan <jgarcia@ced.uab.es>
2022-02-09 08:51:30 +0100

    Elechs

--- generate_xml_and_other_examples/format_fix_example_VIV.py
+++ generate_xml_and_other_examples/format_fix_example_VIV.py
@@ -185,11 +185,11 @@ def main(template_file, file_to_fix):
 
 if __name__ == '__main__':
 
-    projects = {'VIV2018.xml','VIV2017.xml','VIV2016.xml','VIV2015.xml','VIV2014.xml','VIV2013.xml','VIV2012.xml','VIV2011.xml','VIV2010.xml','VIV2009.xml','VIV2008.xml','VIV2007.xml','VIV2006.xml'}
+    projects = {'VIV2020.xml','VIV2018.xml','VIV2017.xml','VIV2016.xml','VIV2015.xml','VIV2014.xml','VIV2013.xml','VIV2012.xml','VIV2011.xml','VIV2010.xml'}
 
     project_template = 'VIV2019.xml'
 
-    working_dir = r'C:\Users\jgarcia\Documents\CEDMetadata'
+    working_dir = r'C:\CEDMetadata'
 
     for project in projects:
         print(f'Editing {project}')
