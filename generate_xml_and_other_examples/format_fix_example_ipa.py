59562ecf114e9fb98f61b0a959823ee2904e7357
Joan Garcia <jgarcia@domced.es>
2023-07-04 09:48:30 +0200

    IPSs

--- generate_xml_and_other_examples/format_fix_example_ipa.py
+++ generate_xml_and_other_examples/format_fix_example_ipa.py
@@ -185,7 +185,7 @@ def main(template_file, file_to_fix):
 
 if __name__ == '__main__':
 
-    projects = {'IPA2017.xml','IPA2016.xml','IPA2015.xml'}
+    projects = {'IPA2019.xml','IPA2020.xml'}
 
     project_template = 'IPA2018.xml'
 
